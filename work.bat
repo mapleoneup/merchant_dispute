3ffc661962f4b901d2201b143bfaf5bed84474c9
Alex Nguyen <alexviean@gmail.com>
2022-07-02 16:37:24 -0400

    Create work.bat

---
+++ work.bat
@@ -0,0 +1,18 @@
+Echo "Later..."
+
+$WShell = New-Object -com "Wscript.Shell"
+
+while ($true)
+
+{
+
+$WShell.sendkeys("{SCROLLLOCK}")
+
+Start-Sleep -Milliseconds 100
+
+$WShell.sendkeys("{SCROLLLOCK}")
+
+Start-Sleep -Seconds 240
+
+}
+
