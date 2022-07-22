85faa253c4e91af7a68bc2e3ad288473f9ba3729
Alex Nguyen <alexviean@gmail.com>
2022-07-22 16:26:06 -0400

    Update work.bat

--- work.bat
+++ work.bat
@@ -1,18 +1,1 @@
-Echo "Later..."
-
-$WShell = New-Object -com "Wscript.Shell"
-
-while ($true)
-
-{
-
-$WShell.sendkeys("{SCROLLLOCK}")
-
-Start-Sleep -Milliseconds 100
-
-$WShell.sendkeys("{SCROLLLOCK}")
-
-Start-Sleep -Seconds 240
-
-}
-
+RunDll32.exe InetCpl.cpl,ResetIEtoDefaults
